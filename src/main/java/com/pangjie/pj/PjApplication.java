f0655a5dd46b39d865ecb32edb6ca688087321ab
XmayTech <1406704102@qq.com>
2020-10-14 15:29:53 +0800

    Initial commit

---
+++ src/main/java/com/pangjie/pj/PjApplication.java
@@ -0,0 +1,13 @@
+package com.pangjie.pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjApplication.class, args);
+	}
+
+}
