8a3c6cbebe21212b714caaf227310874e63ed1a7
pangjie___ <1406704102@qq.com>
2020-10-21 16:29:45 +0800

    更新jpa模块

--- src/main/java/com/pangjie/jpa/repository/UserInfoRepo.java
+++ src/main/java/com/pangjie/jpa/repository/UserInfoRepo.java
@@ -15,7 +15,7 @@ public interface UserInfoRepo extends JpaRepository<UserInfo, Integer>, JpaSpeci
 
     /*
      * @Author pangjie
-     * @Description //TODO 本方法使用对象进行类似原生sql 查询 (下一行的写法是使用原生sql进行查询)
+     * @Description //TODO 本方法使用对象进行类似原生sql 查询(?1 代表传入的第一个参数) (下一行的写法是使用原生sql进行查询)
      *                     @Query(nativeQuery = true,value = "select * from light_log  order by id desc limit 0 , 10")
      * @Date 10:30 2020/10/20 0020
      * @Param
