08a31029dd4c6400625fd40f90795c0034e3327c
pangjie <1406704102@qq.com>
2023-05-16 14:32:11 +0800

    新增 RedisUtils

--- src/main/java/com/pangjie/excel/ExcelController.java
+++ src/main/java/com/pangjie/excel/ExcelController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,18 @@ public class ExcelController {
             //...
         });
         return new ResponseEntity<>(lists, HttpStatus.OK);
+    }
+
+    public static void main(String[] args) {
+        File file = new File("D:\\OtherTool\\WeChat\\WeChat Files\\pangjie123456789\\FileStorage\\File\\2023-05\\pppp.xlsx");
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            WorkbookFactory.create(fis).getSheetAt(0).forEach(cells -> {
+                //...
+            });
+        } catch (IOException | InvalidFormatException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
